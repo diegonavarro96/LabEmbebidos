5ab726c8e00e6950084d806999470af1c4585a9b
diego <diegonavarroarizpe@hotmail.com>
2020-02-28 16:27:22 -0600

    primer commit

---
+++ serial_write.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import time
+import serial
+import sys
+
+ser = serial.Serial('/dev/ttyAMA0',
+ 9600,
+ timeout = 0,
+ bytesize = serial.EIGHTBITS,
+ parity=serial.PARITY_NONE,
+ stopbits = serial.STOPBITS_ONE
+)
+buff=""
+while 1:
+	read = ser.read();
+	#print(read)
+	if read:
+		if read=="\r":
+			l = len(buff)
+			#print(l)
+			#for x in (buff):
+			print(buff);
+			buff=""
+			res = raw_input()
+			for i in range (0,len(res)):
+				ser.write(res[i])
+		else:
+			buff+=(read);
+	#ser.write('a')
+	#time.sleep(1)
+
